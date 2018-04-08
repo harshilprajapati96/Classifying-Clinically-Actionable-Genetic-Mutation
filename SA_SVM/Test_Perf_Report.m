1278674e5940b3d0178879ee8df0ebd73bbe5a1c
bowenislandsong <bowenislandsong@gmail.com>
2018-04-08 09:22:56 -0400

    SA_SVM

--- SA_SVM/Test_Perf_Report.m
+++ SA_SVM/Test_Perf_Report.m
@@ -11,7 +11,7 @@ toc
 % use data with filtered stoped words
 %% Training woth OVO
 tic
-tuning = [150 200]; % tuning prarmeter for US new is suggested to be 150 at best
+tuning = [150]; % tuning prarmeter for US new is suggested to be 150 at best
 for i_tuning = 1:length(tuning)
     disp("X_train preperation time:")
     tic
@@ -29,52 +29,52 @@ toc
 % without parfor
 % with most inner parfor
 % with outter parfor
-%% Training woth OVA
-tic
-svm_group_ova = training_SA_SVM(X_train_woSTOP,Y_train,Y_train_expand,length(vocab),tuning,'ova');
-disp("Training for OVA is done:")
-toc
-%% prepare for test
-[~,~,docIDreorder_test] = unique(X_test(:,1));
-X_test = sparse(docIDreorder_test,X_test(:,2),...
-    X_test(:,3),length(Y_test),length(vocab));
-%% Evaluation
-tic
-
-prediction_ovo = zeros(length(Y_test),length(svm_group_ovo));
-parfor i = 1:length(svm_group_ovo)
-    prediction_ovo(:,i) = svmclassify(svm_group_ovo(i),X_test);
-end
-
-prediction_ovo = mode(prediction_ovo,2);
-ccr_ovo = mean(prediction_ovo==Y_test);
-
-toc
-display(ccr_ovo)
-PreXtruth = confusionmat(prediction_ovo,Y_test);
-display(PreXtruth);
-disp("Evaluation for OVO is done:")
-toc
-
-tic
-prediction_ova = zeros(length(Y_test),length(svm_group_ova));
-parfor i = 1:length(svm_group_ova)
-    prediction_ova(:,i) = svmclassify(svm_group_ova(i),X_test);
-end
-
-prediction_ova = mode(prediction_ova,2);
-ccr_ova = mean(prediction_ova==Y_test);
-
-toc
-display(ccr_ova)
-PreXtruth = confusionmat(prediction_ova,Y_test);
-display(PreXtruth);
-disp("Evaluation for OVA is done:")
-toc
-%% Report CCR
-
-% Confustion matrix based on best CCR
-
-%% Report F-score and recall
-
-% Confustion matrix based on best F-score and recall
+% %% Training woth OVA
+% tic
+% svm_group_ova = training_SA_SVM(X_train_woSTOP,Y_train,Y_train_expand,length(vocab),tuning,'ova');
+% disp("Training for OVA is done:")
+% toc
+% %% prepare for test
+% [~,~,docIDreorder_test] = unique(X_test(:,1));
+% X_test = sparse(docIDreorder_test,X_test(:,2),...
+%     X_test(:,3),length(Y_test),length(vocab));
+% %% Evaluation
+% tic
+% 
+% prediction_ovo = zeros(length(Y_test),length(svm_group_ovo));
+% parfor i = 1:length(svm_group_ovo)
+%     prediction_ovo(:,i) = svmclassify(svm_group_ovo(i),X_test);
+% end
+% 
+% prediction_ovo = mode(prediction_ovo,2);
+% ccr_ovo = mean(prediction_ovo==Y_test);
+% 
+% toc
+% display(ccr_ovo)
+% PreXtruth = confusionmat(prediction_ovo,Y_test);
+% display(PreXtruth);
+% disp("Evaluation for OVO is done:")
+% toc
+% 
+% tic
+% prediction_ova = zeros(length(Y_test),length(svm_group_ova));
+% parfor i = 1:length(svm_group_ova)
+%     prediction_ova(:,i) = svmclassify(svm_group_ova(i),X_test);
+% end
+% 
+% prediction_ova = mode(prediction_ova,2);
+% ccr_ova = mean(prediction_ova==Y_test);
+% 
+% toc
+% display(ccr_ova)
+% PreXtruth = confusionmat(prediction_ova,Y_test);
+% display(PreXtruth);
+% disp("Evaluation for OVA is done:")
+% toc
+% %% Report CCR
+% 
+% % Confustion matrix based on best CCR
+% 
+% %% Report F-score and recall
+% 
+% % Confustion matrix based on best F-score and recall
