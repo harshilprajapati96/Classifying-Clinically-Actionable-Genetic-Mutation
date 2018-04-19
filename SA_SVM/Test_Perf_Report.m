47885d19e8475e715c5512402da41985a1d79b96
bowenislandsong <bowenislandsong@gmail.com>
2018-04-18 22:47:38 -0400

    taking a step back to normal

--- SA_SVM/Test_Perf_Report.m
+++ SA_SVM/Test_Perf_Report.m
@@ -33,11 +33,11 @@ star_tuning = 1; % should be set to the best cv-CCR
 X_test_processed = Norm_preprocessing(X_test_woSTOP,length(vocab));
 disp("Decising time:")
 
-label_predict = zeros(length(svm_group_ova),1);
+prediction = zeros(length(svm_group_ova),1);
 for i = 1:length(svm_group_ova)
-prediction = testing_SA_SVM(X_test_processed,svm_group_ova{i});
-label_predict(i) = mode(prediction,2);
+prediction(i) = testing_SA_SVM(X_test_processed,svm_group_ova{i});
 end
+prediction = mode(prediction,2);
 ccr = mean(Y_train==label_predict);
 disp("Training for OVO is done:")
 toc
