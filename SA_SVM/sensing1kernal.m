e58d5c5bd3f0e01a795441d5fc4b48118246665a
bowenislandsong <bowenislandsong@gmail.com>
2018-04-29 19:40:04 -0400

    working SASVM

--- SA_SVM/sensing1kernal.m
+++ SA_SVM/sensing1kernal.m
@@ -9,7 +9,7 @@ function K1 = sensing1kernal(x_hat_i,x_hat_j)
 K1 = zeros(size(x_hat_i,1),size(x_hat_j,1));
 global SA_n
 
-parfor i = 1:size(x_hat_i,1)
+for i = 1:size(x_hat_i,1)
 K1(i,:) = sum(gammaln(SA_n.*x_hat_i(i,:)+SA_n.*x_hat_j+1)-gammaln(SA_n.*x_hat_i(i,:)+1)-gammaln(SA_n.*x_hat_j+1),2);
 
 end
